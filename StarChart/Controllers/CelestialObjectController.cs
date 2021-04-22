193a54a498e7419fb5a0f829fb1a65922e696563
Wayne Wolf <wjwolf79@gmail.com>
2021-04-22 19:53:10 -0400

    Update CelestialObjectController.cs

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -16,6 +18,30 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet("id:int",Name ="GetById")]
+        public IActionResult GetbyId(int id)
+        {
+            var co = _context.CelestialObjects.Find(id);
+            if(co == null) return NotFound();
+
+            co.Satellites = (List<CelestialObject>)from o in _context.CelestialObjects
+                            where o.OrbitedObjectId.Equals(co.Id)
+                            select o;
+            return Ok(co);
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var co = _context.CelestialObjects.FirstOrDefault(o => o.Name == name);
+            if (co.Name != name) return NotFound();
+
+            co.Satellites = (List<CelestialObject>)from o in _context.CelestialObjects
+                                                   where o.OrbitedObjectId.Equals(co.Id)
+                                                   select o;
+            return Ok(co);
         }
     }
 }
